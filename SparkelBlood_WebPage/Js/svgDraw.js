b571a52eda2a574252403266512f4bfb1530c22f
IndiaRozee <india.rozee@lethpolytech.ca>
2026-03-18 20:08:31 -0600

    Delete SparkelBlood_WebPage directory

--- SparkelBlood_WebPage/Js/svgDraw.js
+++
@@ -1,9 +0,0 @@
-// use a script tag or an external JS file
-document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(DrawSVGPlugin);
-  // gsap code here!
-  gsap.set("#sparkle, #blood", { visibility: "visible" });
-
-  gsap.from("#sparkle .st1", 2, { drawSVG: 0 });
-  gsap.from("#blood .st2", 2, { drawSVG: 0 });
-});
